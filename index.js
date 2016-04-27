aefb34be833af57d115e1c9db114ffe067782caf
Daniel Sellers <daniel@designfrontier.net>
2016-04-27 08:03:09 -0600

    Gets things running and rolling
    Adds the base functionality... needs lots more work though

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-var packages = require('package.json');
+#!/usr/bin/env node
+'use strict';
+var path = require('path')
+  , cp = require('child_process')
+  , fs = require('fs')
+  , isProblematic = function (name) {
+    var badDeps = ['fsevents', 'canvas', 'dbus'];
 
-console.log(packages);
+    return badDeps.indexOf === -1;
+  }
+
+  , cleanDependencies = function (depObject) {
+    return Object.keys(depObject).reduce(function (result, key) {
+      if ( depObject[key].dependencies) {
+        result[key] = cleanDependencies(depObject[key].dependencies);
+      } else {
+        if (!isProblematic(key)) {
+          result[key] = depObject[key];
+        } else {
+          console.log('Bad DEP!', key);
+        }
+      }
+
+      return result;
+    }, {});
+  };
+
+cp.exec('npm shrinkwrap --dev', function (err, stdout, stderr) {
+  var shrinkwrapped = require(path.join(process.cwd(), './npm-shrinkwrap.json'))
+    , clean = cleanDependencies(shrinkwrapped.dependencies);
+
+  fs.writeFile(path.join(process.cwd(), './npm-shrinkwrap.json'), JSON.stringify(clean));
+  fs.writeFile(path.join(process.cwd(), './npm-shrinkwrap.unsafe.json'), JSON.stringify(shrinkwrapped));
+});
